6b259be4b4ab399e2fafc04e3ae47571e30263a8
Omar Semgey <omar.semgey@gmail.com>
2025-12-19 00:43:37 +0300

    Bug Fix: Faulty Theme Detector

--- src/hooks/useDetectTheme.jsx
+++ src/hooks/useDetectTheme.jsx
@@ -5,17 +5,17 @@ export default function useDetectTheme() {
         const theme = localStorage.getItem('theme');
 
         if(theme == 'dark'){
+            localStorage.setItem('theme','dark');
+            
+            document.body.classList.remove('light');
+            
+            document.body.classList.add('dark');
+        }else if(theme == 'light'){
             localStorage.setItem('theme','light');
 
             document.body.classList.remove('dark');
     
             document.body.classList.add('light');
-        }else if(theme == 'light'){
-            localStorage.setItem('theme','dark');
-
-            document.body.classList.remove('light');
-    
-            document.body.classList.add('dark');
         }
     },[]);
 }
