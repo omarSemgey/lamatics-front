21073890c011e55b91e41f095806667c8f227952
Omar Semgey <omar.semgey@gmail.com>
2025-12-18 23:56:43 +0300

    Fixing Bugs

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import ErrorBoundary from './components/ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary.jsx';
 import App from './App.jsx'
 import './api/client'; 
 import './general.css'
