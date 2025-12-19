377b4cb964d0363dff59c6e49b6d5e18d5e8806a
Omar Semgey <omar.semgey@gmail.com>
2025-12-19 23:01:18 +0300

    Fixing Bugs

--- src/components/Dashboard/Forms/Create/CreateQuiz/CreateQuiz.jsx
+++ src/components/Dashboard/Forms/Create/CreateQuiz/CreateQuiz.jsx
@@ -154,7 +154,7 @@ export default function CreateQuiz() {
 
             [0, 1, 2, 3].forEach((answerIndex) => {
                 const answerText = answerRefs.current[questionIndex]?.[answerIndex]?.value || '';
-                const isCorrect = selectedAnswers[questionIndex] === answerIndex ? '1' : '0';
+                const isCorrect = selectedAnswers[questionIndex] === answerIndex
 
                 formData.append(`questions[${questionIndex}][answers][${answerIndex}][question_answer_text]`, answerText);
                 formData.append(`questions[${questionIndex}][answers][${answerIndex}][correct_answer]`, isCorrect);
